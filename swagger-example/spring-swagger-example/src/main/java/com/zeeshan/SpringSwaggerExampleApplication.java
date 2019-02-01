491ab94492a27cf3cd4abf85cfee629e520f33b0
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2019-02-01 18:46:23 +0530

    new code for application

--- swagger-example/spring-swagger-example/src/main/java/com/zeeshan/SpringSwaggerExampleApplication.java
+++ swagger-example/spring-swagger-example/src/main/java/com/zeeshan/SpringSwaggerExampleApplication.java
@@ -8,5 +8,11 @@ public class SpringSwaggerExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSwaggerExampleApplication.class, args);
+
+		public String dummy(){
+			return "dummy";
 	}
 }
+//very initial file to work with
+//run application as 
+//http://localhost:9090/swagger-ui.html
