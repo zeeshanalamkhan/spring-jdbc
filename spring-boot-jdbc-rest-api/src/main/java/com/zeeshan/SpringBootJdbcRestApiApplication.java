c89b05f9c740aa5ebfe00ff3eb08e0e20ddc3ad8
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-02-06 15:39:01 +0530

    SpringBootJdbcRestApiApplication class containing main(-) method

---
+++ spring-boot-jdbc-rest-api/src/main/java/com/zeeshan/SpringBootJdbcRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.zeeshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcRestApiApplication.class, args);
+	}
+
+}
